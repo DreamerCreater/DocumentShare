a03979db7aa79c8c1f6a7f456d86133c6de375c7
GerleeC <ch.gerlee01@gmail.com>
2021-10-09 22:38:09 +0800

    Created new BookShare App

---
+++ BookShare/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using BookShare.Data.Models;
+using BookShare.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShare.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private IBookService _service;
+
+        public BooksController(IBookService service)
+        {
+            _service = service;
+        }
+        //Create/Add A NEW BOOK
+        [HttpPost("AddBook")]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            _service.AddBook(book);
+            return Ok("Added");
+        }
+        //Read all Books
+        [HttpGet("[action]")]
+        public IActionResult GetBooks()
+        {
+            var allBooks = _service.getAllBooks();
+            return Ok(allBooks);
+        }
+        //Update an existing book
+        [HttpPut("UpdateBook/{id}")]
+        public IActionResult UpdateBook(int id,[FromBody] Book book)
+        {
+            _service.UpdateBook(id, book);
+            return Ok(book);
+        }
+        //Delete a book
+        [HttpDelete("DeleteBook/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _service.DeleteBook(id);
+            return Ok();
+        }
+       //get a single boook by id
+       [HttpGet("SingleBook/{id}")]
+       public IActionResult GetBookById(int id)
+        {
+            var book = _service.GetBookById(id);
+            return Ok(book);
+        }
+    }
+}
---
+++ BookShare/Data/Services/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using BookShare.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShare.Data.Services
+{
+    public interface IBookService
+    {
+        List<Book> getAllBooks();
+        Book GetBookById(int id);
+        void UpdateBook(int id, Book newBook);
+        void DeleteBook(int id);
+        void AddBook(Book newBook);
+
+    }
+}
